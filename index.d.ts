dea203663cc23579e5e51b1410b72d6d50f31cdf
Himmet Avsar <avsar.himmet1@gmail.com>
2017-03-13 19:50:58 +0100

    Fix definition file and export

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
+import { RedisStorage } from './src/storages/RedisStorage';
 import { FsJsonStorage } from './src/storages/FsJsonStorage';
 import { MemoryStorage } from './src/storages/MemoryStorage';
 import { ExpirationStrategy } from './src/strategies/ExpirationStrategy';
 import { Cache } from './src/CacheDecorator';
 import { AbstractBaseStrategy } from './src/strategies/AbstractBaseStrategy';
 
-export { Cache, ExpirationStrategy, MemoryStorage, FsJsonStorage };
+export { Cache, ExpirationStrategy, MemoryStorage, FsJsonStorage, RedisStorage };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import { RedisStorage } from './storages/RedisStorage';
 import { FsJsonStorage } from './storages/FsJsonStorage';
 import { MemoryStorage } from './storages/MemoryStorage';
 import { ExpirationStrategy } from './strategies/ExpirationStrategy';
 import { Cache } from './CacheDecorator';
 import { AbstractBaseStrategy } from './strategies/AbstractBaseStrategy';
 
-export { Cache, ExpirationStrategy, MemoryStorage, FsJsonStorage };
+export { Cache, ExpirationStrategy, MemoryStorage, FsJsonStorage, RedisStorage };
