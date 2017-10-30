9bd1bb279a9dcd337961af573a347929348c792e
Himmet Avsar <avsar.himmet1@gmail.com>
2017-10-30 15:44:10 +0100

    Typings fix

--- src/strategies/AbstractBaseStrategy.ts
+++ src/strategies/AbstractBaseStrategy.ts
@@ -5,6 +5,6 @@ export abstract class AbstractBaseStrategy {
     constructor(protected storage: IStorage) { }
 
     public async abstract getItem<T>(key: string): Promise<T>;
-    public async abstract setItem(key: string, content: any, options: object): Promise<void>;
+    public async abstract setItem(key: string, content: any, options: any): Promise<void>;
     public async abstract clear(): Promise<void>;
 }
