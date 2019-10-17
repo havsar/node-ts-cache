0214523c4b219c223668cbf7be5821054a139811
simon <s.tretter@gmail.com>
2019-10-17 18:30:12 +0200

    fix(export): export abstract base strategy

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import {FsJsonStorage} from './storages/FsJsonStorage';
 import {MemoryStorage} from './storages/MemoryStorage';
 import {ExpirationStrategy} from './strategies/ExpirationStrategy';
 import {Cache} from './CacheDecorator';
+import {AbstractBaseStrategy} from './strategies/AbstractBaseStrategy';
 
-export {Cache, ExpirationStrategy, MemoryStorage, FsJsonStorage, RedisStorage};
+export {Cache, ExpirationStrategy, MemoryStorage, FsJsonStorage, RedisStorage, AbstractBaseStrategy};
