940076c55581dafa6c4c1f6a4f34c8f97746f6c8
simon <simon.tretter@hokify.com>
2020-03-06 15:52:32 +0100

    refactor: use ioredis instead of redis client lib
    fixes also type defs, async stuff and moves deps to peerdependencies

--- test/redis.storage.test.ts
+++ test/redis.storage.test.ts
@@ -1,35 +1,19 @@
 import * as Proxyquire from 'proxyquire'
-import * as Sinon from 'sinon'
 import * as Assert from 'assert'
 import { RedisStorage } from '../src'
 
 Proxyquire.noCallThru()
 
-const clientMock = {
-    flushdbAsync: Sinon.fake(),
-    setItem: Sinon.fake(),
-    delAsync: Sinon.fake(),
-    getAsync: Sinon.fake()
-}
+// @ts-ignore
+import * as  RedisMock from 'ioredis-mock';
 
-const RedisMock = {
-    createClient: () => clientMock,
-    RedisClient: {
-        prototype: {}
-    },
-    Multi: {
-        prototype: {}
-    }
-}
-const MockRedisStorage: typeof RedisStorage = Proxyquire.load('../src/storage/redis.storage', {
-    'redis': RedisMock
-}).RedisStorage
-
-const storage = new MockRedisStorage({
+const MockedRedis = new RedisMock({
     host: 'host',
     port: 123,
     password: 'pass'
-})
+});
+
+const storage = new RedisStorage({}, MockedRedis);
 
 describe('RedisStorage', () => {
     it('Should clear Redis without errors', async () => {
@@ -39,11 +23,8 @@ describe('RedisStorage', () => {
     it('Should delete cache item if set to undefined', async () => {
         await storage.setItem('test', undefined)
 
-        Assert.strictEqual(clientMock.delAsync.called, true)
-        Assert.strictEqual(clientMock.delAsync.calledWith('test'), true)
-        Assert.strictEqual(clientMock.setItem.called, false)
+        Assert.strictEqual(await storage.getItem('test') , undefined);
     })
-
 
     it('Should return undefined if cache not hit', async () => {
         await storage.clear()
@@ -51,44 +32,5 @@ describe('RedisStorage', () => {
 
         Assert.strictEqual(item, undefined)
     })
-
-    it('Should throw an Error if connection to redis fails', async () => {
-        const clientMock = {
-            flushdbAsync: Sinon.stub().rejects(new Error('Redis connection failed')),
-            setItem: Sinon.fake(),
-            delAsync: Sinon.fake(),
-            getAsync: Sinon.fake()
-        }
-        const RedisMock = {
-            createClient: () => clientMock,
-            RedisClient: {
-                prototype: {}
-            },
-            Multi: {
-                prototype: {}
-            }
-        }
-        const MockRedisFailStorage: typeof RedisStorage = Proxyquire.load('../src/storage/redis.storage', {
-            'redis': RedisMock
-        }).RedisStorage
-
-        const testStorage = new MockRedisFailStorage({
-            host: 'unknown-host',
-            port: 123,
-            password: 'pass',
-            connect_timeout: 1000
-        })
-
-        const errorMsg = 'Should have thrown an error, but did not'
-        try {
-            await testStorage.clear()
-            await Promise.reject(errorMsg)
-        } catch (error) {
-            if (error === errorMsg) {
-                Assert.fail('It id not throw an error')
-            } else {
-                Assert.ok(true)
-            }
-        }
-    })
 })
+
