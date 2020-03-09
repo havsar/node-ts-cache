e1cdd97e1238f1c0ee71b703d14086ce5158b4e0
simon <simon.tretter@hokify.com>
2020-03-09 12:31:28 +0100

    fix(exports): add memory and fs storage exports

--- ts-cache/src/index.ts
+++ ts-cache/src/index.ts
@@ -3,3 +3,6 @@ export { ExpirationStrategy } from "./strategy/caching/expiration.strategy";
 export { IKeyStrategy } from "./strategy/key/key.strategy.types";
 export { ICacheStrategy } from "./strategy/caching/cache.strategy.types";
 export { Cache } from "./decorator/cache.decorator";
+
+export { FsJsonStorage } from "./storage/fs";
+export { MemoryStorage } from "./storage/memory";
